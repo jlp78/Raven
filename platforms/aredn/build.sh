604c8496296e62525e21a548c885ae58590f908c
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2026-03-12 16:33:25 -0700

    Build IPK and APK packages

--- platforms/aredn/build.sh
+++ platforms/aredn/build.sh
@@ -1,42 +1,22 @@
 #! /bin/sh
 
-VERSION=0.0.1-r$(($(date +%s) - $(date -d "2026-01-01 00:00:00" +%s)))
+VER=0.0.1
+REL=r$(($(date +%s) - $(date -d "2026-01-01 00:00:00" +%s)))
+VERSION=${VER}-${REL}
 
 ROOT=/tmp/raven-build-$$
 SRC=$(dirname $0)/../..
 
 rm -rf $ROOT/
 
-mkdir -p $ROOT/control \
-    $ROOT/data/www/cgi-bin/apps/raven \
+mkdir -p $ROOT/data/www/cgi-bin/apps/raven \
     $ROOT/data/www/apps/raven \
-    $ROOT/tmp/apps/raven \
     $ROOT/data/usr/local/raven/platforms/aredn $ROOT/data/usr/local/raven/crypto \
     $ROOT/data/etc/init.d \
     $ROOT/data/etc/local/mesh-firewall \
     $ROOT/data/etc/arednsysupgrade.d
 
-cat > $ROOT/debian-binary <<__EOF__
-2.0
-__EOF__
-cat > $ROOT/control/control <<__EOF__
-Package: raven
-Version: ${VERSION}
-Depends: ucode, curl
-Provides:
-Source: package/raven
-Section: net
-Priority: optional
-Maintainer: Tim Wilkinson (KN6PLV)
-Architecture: all
-Description: Mesh communications
-__EOF__
-
-cp $SRC/platforms/aredn/postinst $ROOT/control/postinst
-cp $SRC/platforms/aredn/prerm $ROOT/control/prerm
 cp $SRC/platforms/aredn/firewall $ROOT/data/etc/local/mesh-firewall/21-raven
-
-chmod 755 $ROOT/control/postinst $ROOT/control/prerm $ROOT/data/etc/local/mesh-firewall/21-raven
 
 cp $SRC/*.uc $ROOT/data/usr/local/raven/
 cp $SRC/crypto/*.uc $ROOT/data/usr/local/raven/crypto/
@@ -53,10 +33,34 @@ cp $SRC/platforms/aredn/raven.init $ROOT/data/etc/init.d/raven
 
 cp $SRC/platforms/aredn/upgrade.conf $ROOT/data/etc/arednsysupgrade.d/KN6PLV.raven.conf
 
+chmod 755 $ROOT/data/etc/local/mesh-firewall/21-raven
 chmod 755 $ROOT/data/www/apps/raven/* $ROOT/data/www/cgi-bin/apps/raven/admin $ROOT/data/www/cgi-bin/apps/raven/image
 
 mkdir -p $ROOT/data/usr/local/raven/winlink/forms
 cp -R $SRC/winlink/forms/* $ROOT/data/usr/local/raven/winlink/forms
+
+#
+# Make IPKG
+#
+mkdir -p $ROOT/control
+cat > $ROOT/debian-binary <<__EOF__
+2.0
+__EOF__
+cat > $ROOT/control/control <<__EOF__
+Package: raven
+Version: ${VERSION}
+Depends: ucode, curl
+Provides:
+Source: package/raven
+Section: net
+Priority: optional
+Maintainer: Tim Wilkinson (KN6PLV)
+Architecture: all
+Description: Mesh communications
+__EOF__
+cp $SRC/platforms/aredn/postinst $ROOT/control/postinst
+cp $SRC/platforms/aredn/prerm $ROOT/control/prerm
+chmod 755 $ROOT/control/postinst $ROOT/control/prerm
 
 (cd $ROOT/control ; tar cfz ../control.tar.gz .)
 (cd $ROOT/data ; tar cfz ../data.tar.gz .)
@@ -64,4 +68,25 @@ cp -R $SRC/winlink/forms/* $ROOT/data/usr/local/raven/winlink/forms
 
 rm -f ./raven_*_all.ipk
 mv $ROOT/raven_${VERSION}_all.ipk .
+
+#
+# Make APK
+#
+rm -f ./raven-*.apk
+cp $SRC/platforms/aredn/postinst $ROOT/data/.post-install
+cp $SRC/platforms/aredn/prerm $ROOT/data/.pre-deinstall
+chmod 755 $ROOT/data/.post-install $ROOT/data/.pre-deinstall
+mkapk.py \
+    -n raven \
+    -v ${VER} \
+    -d ${ROOT}/data \
+    -a noarch \
+    -r ${REL} \
+    -D 'Raven Mesh Messaging' \
+    -u 'https://github.com/kn6plv/raven' \
+    -l 'MIT' \
+    -m 'tim.j.wilkinson@gmail.com' \
+    -p ucode,curl \
+    -o .
+
 rm -rf $ROOT/
