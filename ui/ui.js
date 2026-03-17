0b53fc47aee470fa33a70fd9ae1e9a477a7e6fb9
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2026-03-17 14:53:47 -0700

    Q traffic when socket is down

--- ui/ui.js
+++ ui/ui.js
@@ -1,5 +1,4 @@
 let sock = null;
-let send = () => {};
 let rightSelection = null;
 let previousSelection = null;
 let channels = null;
@@ -17,6 +16,7 @@ let winlink = null;
 let activityTimeout;
 let catchupTimeout;
 let focusid = null;
+let sq = [];
 const xdiv = document.createElement("div");
 
 const sha256 = function a(b){function c(a,b){return a>>>b|a<<32-b}for(var d,e,f=Math.pow,g=f(2,32),h="length",i=[],j=[],k=8*b[h],l=a.h=a.h||[],m=a.k=a.k||[],n=m[h],o={},p=2;64>n;p++)if(!o[p]){for(d=0;313>d;d+=p)o[d]=p;l[n]=f(p,.5)*g|0,m[n++]=f(p,1/3)*g|0}for(b+="\x80";b[h]%64-56;)b+="\x00";for(d=0;d<b[h];d++){if(e=b.charCodeAt(d),e>>8)return;j[d>>2]|=e<<(3-d)%4*8}for(j[j[h]]=k/g|0,j[j[h]]=k,e=0;e<j[h];){var q=j.slice(e,e+=16),r=l;for(l=l.slice(0,8),d=0;64>d;d++){var s=q[d-15],t=q[d-2],u=l[0],v=l[4],w=l[7]+(c(v,6)^c(v,11)^c(v,25))+(v&l[5]^~v&l[6])+m[d]+(q[d]=16>d?q[d]:q[d-16]+(c(s,7)^c(s,18)^s>>>3)+q[d-7]+(c(t,17)^c(t,19)^t>>>10)|0),x=(c(u,2)^c(u,13)^c(u,22))+(u&l[1]^u&l[2]^l[1]&l[2]);l=[w+x|0].concat(l),l[4]=l[4]+w|0}for(d=0;8>d;d++)l[d]=l[d]+r[d]|0}for(d=0;8>d;d++)for(e=3;e+1;e--)i.push(l[d]>>8*e&255);return i};
@@ -25,6 +25,12 @@ function bytesToBase64(bytes)
 {
     return btoa(Array.from(bytes, byte => String.fromCodePoint(byte)).join(""));
 }
+
+function sendq(msg)
+{
+    sq.push(msg);
+}
+let send = sendq;
 
 const roles = {
     0: "Client",
@@ -1103,7 +1109,7 @@ function restartup()
         catch (_) {
         }
         sock = null;
-        send = () => {};
+        send = sendq;
         setTimeout(startup, 2000);
     }
 }
@@ -1120,6 +1126,8 @@ function startup()
     sock.addEventListener("open", _ => {
         activity();
         send = (msg) => sock.send(msg instanceof Blob ? msg : JSON.stringify(msg));
+        sq.forEach(send);
+        sq = [];
     });
     sock.addEventListener("close", restartup);
     sock.addEventListener("error", restartup);
